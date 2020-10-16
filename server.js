f689660c88422ecc8598f14feeb9635ae9c8cdce
Human Inhabitant <human-inhabitant@pm.me>
2020-10-16 16:33:32 -0400

    Updated server.js...

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const debug = require('debug')('server');
+const debug = require('debug')('app');
 
 const app = express();
 const port = process.env.PORT || 3e3;
